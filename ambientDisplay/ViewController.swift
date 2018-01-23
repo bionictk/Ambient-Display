6b2095b2f4c09aa44d66d51fd6d99b90d9f75eeb
Taeheon Kim <bionictk@gmail.com>
2018-01-23 17:06:16 -0500

    Add Label for error display

--- ambientDisplay/ViewController.swift
+++ ambientDisplay/ViewController.swift
@@ -8,14 +8,25 @@
 
 import UIKit
 
+extension Notification.Name {
+    static let errorChannel = Notification.Name("errorChannel")
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Listener for errors
+        // Usage: NotificationCenter.default.post(name: .errorChannel, object: "Error message.")
+        NotificationCenter.default.addObserver(self, selector: #selector(setErrorMessage(notification:)), name: .errorChannel, object: nil)
         
+    @objc func setErrorMessage(notification: NSNotification) {
+        errorLabel.text = (notification.object as! String)
+    }
+    
     }
 
 }
